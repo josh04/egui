abd68d2d47529010efd769bc58219f7c3b7ee88a
Josh McNamee <josh04@gmail.com>
2022-12-18 18:20:41 +0000

    vertical bottom up

--- crates/egui/src/ui.rs
+++ crates/egui/src/ui.rs
@@ -1906,6 +1906,10 @@ impl Ui {
     pub fn vertical<R>(&mut self, add_contents: impl FnOnce(&mut Ui) -> R) -> InnerResponse<R> {
         self.with_layout_dyn(Layout::top_down(Align::Min), Box::new(add_contents))
     }
+    #[inline]
+    pub fn vertical_bottom_up<R>(&mut self, add_contents: impl FnOnce(&mut Ui) -> R) -> InnerResponse<R> {
+        self.with_layout_dyn(Layout::bottom_up(Align::Min), Box::new(add_contents))
+    }
 
     /// Start a ui with vertical layout.
     /// Widgets will be horizontally centered.
